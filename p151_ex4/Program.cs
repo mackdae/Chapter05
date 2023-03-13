41f3cf612a6a4b1970fe8f01ada127bee5fee9f3
mackdae <millim0821@naver.com>
2023-03-13 15:50:03 +0900

    230313 pm03:50 문제2

---
+++ p151_ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p151_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문제2
+            //정수 한 개를 입력받아서, 그 수가 3의 배수인지 판단하는 프로그램
+
+            Console.Write("정수 한 개를 입력하세요. : ");
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+            Console.WriteLine("입력하신 정수 {0} 은(는) 3의 배수{1}니다.", number, number % 3 == 0 ? "입" : "가 아닙");
+        }
+    }
+}
